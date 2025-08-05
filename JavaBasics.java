9438e05b1126e3a7cbffe6bc4331194ec9dd27e6
Vaishnavi Mane <vaishnumane2003@gmail.com>
2025-08-05 20:10:30 +0530

    add the code

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,5 +1,15 @@
 public class JavaBasics{
     public static void main (String args[]){
         System.out.println("hello world");
+
+        int a = 10;
+        int b = 5;
+        String name = "apply";
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(b);
+
+        a = b;
+        System.out.println(a);
     }
 }
